2c885c7e0eec6ed3df07da32b2a93000a467498b
Gherendi <gherearc@gmail.com>
2024-01-23 11:18:46 +0000

    Work to do on button. Worked on content view

--- Yummi/ContentView.swift
+++ Yummi/ContentView.swift
@@ -7,18 +7,58 @@
 
 import SwiftUI
 
+struct Ingredient {
+    let name: String
+    let category: String
+    let Quantity: Int
+    let Unit : Int
+    let ExpiryDate: Date
+    
+    
+    func Display() -> String {
+        return ("Name: \(name) , Quantity: \(Quantity) , Category: \(category) , Unit: \(Unit) , ExpiryDate: \(ExpiryDate)")
+    }
+    
+}
+
+
 struct ContentView: View {
+    @State private var newname = ""
+    @State private var newcategory = ""
+    @State private var newQuantity = 0
+    @State private var newUnit = 0
+    @State private var newExpiryDate = Date()
+    
+
+    
+    let ingredient = Ingredient(name: "Carrot", category: "Vegetable", Quantity: 10, Unit: 2, ExpiryDate: Date())
     var body: some View {
+        Form{
+            Section(header: Text("New Ingredient")) {
+                TextField("name:", text: $newname)
+                TextField("category:", text: $newcategory)
+                Stepper(value: $newQuantity, in: 0...1000, label: {
+                    Text("Quantity: \(newQuantity)")
+                })
+                Stepper(value: $newUnit, in: 1...1000, label: {
+                    Text("Unit: \(newUnit)")
+                })
+                DatePicker("Expiry date:", selection: $newExpiryDate, displayedComponents: .date)            }
+            Section {
+                Button("Submit") {
+                    
+                }
+            }
+        }
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(ingredient.Display())
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView() 
 }
+
+
